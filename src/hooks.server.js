4d765f33533e58d7fb7303de9f41840962cf89d0
JJmoff <jahsfreedman12@gmail.com>
2025-09-03 16:33:11 +0100

    v7

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,14 +1,15 @@
 import PocketBase from 'pocketbase'
 import { serializeNonPOJOs } from '$lib/utils'
-import { PUBLIC_POCKETBASE_URL } from '$env/static/public'
+import * as env from '$env/static/public'
+
+const pocketbaseUrl = env.PUBLIC_POCKETBASE_URL || 'http://127.0.0.1:8090'
 
 /** @type {import('@sveltejs/kit').Handle} */
 export const handle = async ({ event, resolve }) => {
-    event.locals.pb = new PocketBase(PUBLIC_POCKETBASE_URL || 'http://127.0.0.1:8090')
+    event.locals.pb = new PocketBase(pocketbaseUrl)
     event.locals.pb.authStore.loadFromCookie(event.request.headers.get('cookie') || '')
 
     if (event.locals.pb.authStore.isValid && event.locals.pb.authStore.model) {
-        // Add a check for authStore.model here
         event.locals.user = serializeNonPOJOs(event.locals.pb.authStore.model)
     } else {
         event.locals.user = undefined
@@ -16,7 +17,10 @@ export const handle = async ({ event, resolve }) => {
 
     const response = await resolve(event)
 
-    response.headers.set('set-cookie', event.locals.pb.authStore.exportToCookie({ secure: false }))
+    response.headers.set(
+        'set-cookie',
+        event.locals.pb.authStore.exportToCookie({ secure: false })
+    )
 
     return response
-};
+}
