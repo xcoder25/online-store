edab48b22e24b1e9cf81209ec7961c64a70f3e01
JJmoff <jahsfreedman12@gmail.com>
2025-09-04 23:35:11 +0100

    aa

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
   preprocess: vitePreprocess(),
